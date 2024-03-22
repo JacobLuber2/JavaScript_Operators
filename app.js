63834134bbfc9c00c24dd4a31271fd49335ada9b
JacobLuber2 <jaluber10@gmail.com>
2024-03-21 18:24:56 -0600

    finished

--- app.js
+++ app.js
@@ -1,19 +1,43 @@
-console.log("Hello World!\n==========\n");
-console.log(
-  "Follow the steps in the README.md file to complete the exercises:\n==========\n"
-);
 
 // Exercise 1
-console.log("EXERCISE 1:\n==========\n");
-
-// YOUR CODE HERE
-
+let a = 20;
+let b = 4;
+let add = a + b;
+let minus = a - b;
+let multiply = a * b;
+let dividing = a / b;
+console.log(add);
+console.log(minus);
+console.log(multiply);
+console.log(dividing);
 // Exercise 2
-console.log("EXERCISE 2:\n==========\n");
-
-// YOUR CODE HERE
+let num = 11;
+let str = "11";
+let str2 = "eleven";
+let isPresent = true;
+let firstName = "Frodo";
+let lastName = "Baggins";
+console.log(num + str, "num + string");
+console.log(num + str2, "num + string");
+console.log(num + isPresent, "num + isPresent");
+console.log(firstName + num, "firstname + num");
+console.log(isPresent + str, "ispresent + str");
+console.log(firstName + lastName, "firstName + lastName");
 
 // Exercise 3
-console.log("EXERCISE 3:\n==========\n");
+let val = 5;
+let str3 = "5";
+let str4 = "five";
+let isPresent2 = false;
 
-// YOUR CODE HERE
+console.log(val == str3, "val == str3");
+console.log(val === str, "val === str3");
+console.log(!isPresent2, "!isPresent2");
+console.log("eleven" === str4 && val >= str3, "eleven” == str4 && val >= str3");
+console.log(!isPresent2 || isPresent2, "!isPresent2 || isPresent2");
+console.log(0 == false, "0 == false");
+console.log(0 === false, "0 === false");
+console.log(0 != false, "0 != false");
+console.log(0 !== false, "0 !== false");
+
+
